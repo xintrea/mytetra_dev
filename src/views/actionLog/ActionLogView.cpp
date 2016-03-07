097aae4596b5cda3c00f962092a1263d9e9f7091
xintrea <xintrea@gmail.com>
2016-03-07 19:16:37 +0300

    Сделана установка ширины окна лога в 80% от MainWindow, иначе лог открывался в слишком маленьком окне

--- src/views/actionLog/ActionLogView.cpp
+++ src/views/actionLog/ActionLogView.cpp
@@ -7,14 +7,27 @@
 #include "ActionLogScreen.h"
 #include "controllers/actionLog/ActionLogController.h"
 #include "libraries/GlobalParameters.h"
+#include "views/mainWindow/MainWindow.h"
+
 
 extern GlobalParameters globalParameters;
 
 
 ActionLogView::ActionLogView(QWidget *parent) : QTableView(parent)
 {
-  this->horizontalHeader()->setStretchLastSection( true );
+  this->horizontalHeader()->setStretchLastSection( true ); // Растягивание последней секции до размеров виджета
   this->setSelectionBehavior(QAbstractItemView::SelectRows); // Выделяется вся строка
+  this->horizontalHeader()->setHighlightSections(false); // Заголовки не должны выглядеть нажатыми
+  this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff); // Отключается горизонтальная прокрутка
+
+  // Установка ширины окна
+  int dialogWidth=int( 0.8 * (float)(find_object<MainWindow>("mainwindow")->width()) );
+  setMinimumWidth( dialogWidth );
+  resize( size() );
+
+  // Установка ширины столбцов
+  // this->horizontalHeader()->setSectionResizeMode(1, QHeaderView::Interactive); // Interactive, ResizeToContents
+  this->resizeColumnToContents(0);
 
   // Настройка области виджета для кинетической прокрутки
   setKineticScrollArea( qobject_cast<QAbstractItemView*>(this) );
@@ -36,9 +49,9 @@ void ActionLogView::setController(ActionLogController *pController)
 void ActionLogView::resizeEvent(QResizeEvent *event)
 {
   // Второй столбец должен занимать 80% ширины при любом размере таблицы
-  QRect viewRect = this->rect();
-  float columnWidth = (float) viewRect.width() * 0.8;
-  this->setColumnWidth( 1, columnWidth );
+  // QRect viewRect = this->rect();
+  // float columnWidth = (float) viewRect.width() * 0.8;
+  // this->setColumnWidth( 1, columnWidth );
 
   // Отрисовка родительского класса
   QTableView::resizeEvent(event);
