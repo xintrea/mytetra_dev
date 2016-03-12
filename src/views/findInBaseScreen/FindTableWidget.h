aafb75f965a0d9579e9beb71bb4b17601a0eeba1
xintrea <xintrea@gmail.com>
2016-03-12 20:03:36 +0300

    Исправлены отступы в классе FindTableWidget

--- src/views/findInBaseScreen/FindTableWidget.h
+++ src/views/findInBaseScreen/FindTableWidget.h
@@ -10,37 +10,37 @@ class QStandardItemModel;
 
 class FindTableWidget : public QWidget
 {
- Q_OBJECT
+  Q_OBJECT
 
 public:
 
- FindTableWidget(QWidget *parent=0);
- virtual ~FindTableWidget(void);
+  FindTableWidget(QWidget *parent=0);
+  virtual ~FindTableWidget(void);
 
- void clearAll(void);
- void addRow(QString title, QString branchName, QString tags, QStringList path, QString recordId);
- int  getRowCount();
- void updateColumnsWidth(void);
- void setOverdrawMessage(const QString iOverdrawMessage); // Установка надписи, которая появляется поверх виджета
+  void clearAll(void);
+  void addRow(QString title, QString branchName, QString tags, QStringList path, QString recordId);
+  int  getRowCount();
+  void updateColumnsWidth(void);
+  void setOverdrawMessage(const QString iOverdrawMessage); // Установка надписи, которая появляется поверх виджета
 
 private slots:
 
- // void selectCell(int row, int column);
- void selectCell(const QModelIndex & index);
+  // void selectCell(int row, int column);
+  void selectCell(const QModelIndex & index);
 
 private:
 
- QTableView *findTableView;
- QStandardItemModel *findTableModel;
- QString overdrawMessage;
- 
- void setupUI(void);
- void setupModels(void);
- void setupSignals(void);
- void assembly(void);
- 
- bool eventFilter( QObject * o, QEvent * e );
- void paintEvent(QPaintEvent *event);
+  QTableView *findTableView;
+  QStandardItemModel *findTableModel;
+  QString overdrawMessage;
+
+  void setupUI(void);
+  void setupModels(void);
+  void setupSignals(void);
+  void assembly(void);
+
+  bool eventFilter( QObject * o, QEvent * e );
+  void paintEvent(QPaintEvent *event);
 
 };
 
