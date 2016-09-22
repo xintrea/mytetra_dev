31d2522f37db029d2554acf23c130d6224f71022
xintrea <xintrea@gmail.com>
2016-09-23 00:56:22 +0300

    Версия 1.32.322 - небольшое исправление TreeItem::removeAllChildren(). Данный метод нигде не используется, но может пригодится в будущем

--- src/main.h
+++ src/main.h
@@ -34,7 +34,7 @@ using namespace std;
 // Версия программы
 #define APPLICATION_RELEASE_VERSION         1
 #define APPLICATION_RELEASE_SUBVERSION     32
-#define APPLICATION_RELEASE_MICROVERSION  321
+#define APPLICATION_RELEASE_MICROVERSION  322
 
 // Поддерживаемая версия формата базы (хранилища)
 #define CURRENT_FORMAT_VERSION    1
