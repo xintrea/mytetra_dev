bc2e5dde6a9586b36d0eb3cafba0b98d5251c578
Sergey Stepanov <xintrea@gmail.com>
2018-09-14 23:55:19 +0300

    Версия v.1.44.29

--- app/src/main.h
+++ app/src/main.h
@@ -34,7 +34,7 @@ using namespace std;
 // Версия программы
 #define APPLICATION_RELEASE_VERSION         1
 #define APPLICATION_RELEASE_SUBVERSION     44
-#define APPLICATION_RELEASE_MICROVERSION   28
+#define APPLICATION_RELEASE_MICROVERSION   29
 
 
 // Поддерживаемая версия формата базы (хранилища)
