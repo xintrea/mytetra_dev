efd822519bf4b93b417fe9547869e55f445cf844
Sergey Stepanov <xintrea@gmail.com>
2018-11-21 23:43:01 +0300

    Добавлено описание шорткатов для активации основных виджетов окна - дерева, списка записей, редактора

--- app/src/main.h
+++ app/src/main.h
@@ -34,7 +34,7 @@ using namespace std;
 // Версия программы
 #define APPLICATION_RELEASE_VERSION         1
 #define APPLICATION_RELEASE_SUBVERSION     44
-#define APPLICATION_RELEASE_MICROVERSION   50
+#define APPLICATION_RELEASE_MICROVERSION   51
 
 
 // Поддерживаемая версия формата базы (хранилища)
