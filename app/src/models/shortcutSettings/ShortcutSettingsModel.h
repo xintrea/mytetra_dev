b28032183080b8f3f63f38374c5799d7a4a47279
xintrea <xintrea@gmail.com>
2018-12-06 12:09:33 +0300

    Стали отображаться разделы настройки клавиатурных сокращений

--- app/src/models/shortcutSettings/ShortcutSettingsModel.h
+++ app/src/models/shortcutSettings/ShortcutSettingsModel.h
@@ -11,17 +11,17 @@ public:
     ShortcutSettingsModel(QObject *parent = nullptr);
     ~ShortcutSettingsModel();
 
-    QVariant data(const QModelIndex &index, int role) const;
-    QVariant headerData(int section, Qt::Orientation orientation,
-                        int role = Qt::DisplayRole) const;
+//    QVariant data(const QModelIndex &index, int role) const;
+//    QVariant headerData(int section, Qt::Orientation orientation,
+//                        int role = Qt::DisplayRole) const;
 
-    int rowCount(const QModelIndex &itemIndex = QModelIndex()) const;
-    int columnCount(const QModelIndex &itemIndex = QModelIndex()) const;
+//    int rowCount(const QModelIndex &itemIndex = QModelIndex()) const;
+//    int columnCount(const QModelIndex &itemIndex = QModelIndex()) const;
 
 
-    Qt::ItemFlags flags(const QModelIndex &index) const;
-    bool setData(const QModelIndex &index, const QVariant &value,
-                 int role = Qt::EditRole);
+//    Qt::ItemFlags flags(const QModelIndex &index) const;
+//    bool setData(const QModelIndex &index, const QVariant &value,
+//                 int role = Qt::EditRole);
 };
 
 #endif // SHORTCUTSETTINGSMODEL_H
