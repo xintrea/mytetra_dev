72d0622330fc2fa599001e0c863df3af31ba13d1
xintrea <xintrea@gmail.com>
2018-12-10 14:07:15 +0300

    Сдан виджет захвата горячих клавищ с кнопкой отмены

--- app/src/views/shortcutSettings/HotKeyGrabber.cpp
+++ app/src/views/shortcutSettings/HotKeyGrabber.cpp
@@ -4,9 +4,11 @@
 #include "HotKeyGrabber.h"
 
 
-HotKeyGrabber::HotKeyGrabber(QWidget* parent) : QKeySequenceEdit(parent)
+HotKeyGrabber::HotKeyGrabber(QWidget* parent) : QWidget(parent)
 {
-
+    this->setupUI();
+    this->setupSignals();
+    this->assembly();
 }
 
 
@@ -16,12 +18,46 @@ HotKeyGrabber::~HotKeyGrabber()
 }
 
 
-void HotKeyGrabber::keyPressEvent( QKeyEvent * event )
+void HotKeyGrabber::setupUI()
 {
-    QKeySequenceEdit::keyPressEvent(event);
+    this->keySequenceEdit=new QKeySequenceEdit(this);
 
-    // Установка значения в виде строки до первой запятой
-    QKeySequence seq( QKeySequence::fromString(keySequence().toString().split(", ").first()) );
-
-    setKeySequence(seq);
+    this->buttonCancel=new QPushButton(this);
+    this->buttonCancel->setText("Cancel");
 }
+
+
+void HotKeyGrabber::setupSignals()
+{
+    // Обработка кнопки Cansel
+    connect(this->buttonCancel, &QPushButton::clicked,
+            this, &HotKeyGrabber::hide);
+
+    // Аналог сигнала QKeySequenceEdit::editingFinished испускается от виджета
+    connect(this->keySequenceEdit, &QKeySequenceEdit::editingFinished,
+            this, &HotKeyGrabber::editingFinished);
+}
+
+
+void HotKeyGrabber::assembly()
+{
+    this->layout=new QHBoxLayout();
+    this->layout->addWidget(this->keySequenceEdit);
+    this->layout->addWidget(this->buttonCancel);
+
+    setLayout(this->layout);
+}
+
+
+void HotKeyGrabber::clear()
+{
+    keySequenceEdit->clear();
+}
+
+
+QKeySequence HotKeyGrabber::keySequence()
+{
+    return keySequenceEdit->keySequence();
+}
+
+
